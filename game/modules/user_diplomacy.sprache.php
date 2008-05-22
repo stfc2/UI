41b45eff08aab21dfabffe7b92b28ce7e912cfe5
merry75 <kirirur@gmail.com>
2008-05-22 18:45:25 +0000

    Aggiunte stringhe italiane a file di localizzazione originari.

--- game/modules/user_diplomacy.sprache.php
+++ game/modules/user_diplomacy.sprache.php
@@ -22,17 +22,17 @@
 //deutsch
 define ("GER_TEXT1",'Diplomatie:');
 
-define ("GER_TEXT2",'Neues Bündnis vorschlagen');
+define ("GER_TEXT2",'Neues B&uuml;ndnis vorschlagen');
 
 define ("GER_TEXT3",'Spielername:');
 
 define ("GER_TEXT4",'Suchen');
 
-define ("GER_TEXT5",'<< Zurück');
+define ("GER_TEXT5",'<< Zur&uuml;ck');
 
 define ("GER_TEXT6",'Bestätigen');
 
-define ("GER_TEXT7",'Mögliche Namen:');
+define ("GER_TEXT7",'M&ouml;gliche Namen:');
 
 define ("GER_TEXT8",'Name:');
 
@@ -42,39 +42,39 @@ define ("GER_TEXT10",'Allianz:');
 
 define ("GER_TEXT11",'Punkte:');
 
-define ("GER_TEXT12",'Bündnis anbieten');
+define ("GER_TEXT12",'B&uuml;ndnis anbieten');
 
 define ("GER_TEXT13",'Kein Spielername angegeben');
 
-define ("GER_TEXT14",'Bündnispartner existiert nicht');
+define ("GER_TEXT14",'B&uuml;ndnispartner existiert nicht');
 
-define ("GER_TEXT15",'Du kannst nicht mit dir selbst ein Bündnis eingehen');
+define ("GER_TEXT15",'Du kannst nicht mit dir selbst ein B&uuml;ndnis eingehen');
 
-define ("GER_TEXT16",'Du hast bereits ein Bündnis/Bündnisangebot mit diesem Spieler');
+define ("GER_TEXT16",'Du hast bereits ein B&uuml;ndnis/B&uuml;ndnisangebot mit diesem Spieler');
 
-define ("GER_TEXT17",'Persönliches Bündnisangebot');
+define ("GER_TEXT17",'Persönliches B&uuml;ndnisangebot');
 
-define ("GER_TEXT18",'Soeben wurde dir ein neues Persönliches Bündnis vorgeschlagen. Schau am Besten in deiner User-Diplomatie nach.');
+define ("GER_TEXT18",'Soeben wurde dir ein neues Persönliches B&uuml;ndnis vorgeschlagen. Schau am Besten in deiner User-Diplomatie nach.');
 
-define ("GER_TEXT19",'Neues Bündnis vorschlagen');
+define ("GER_TEXT19",'Neues B&uuml;ndnis vorschlagen');
 
-define ("GER_TEXT20",'Kein Bündnis angegeben');
+define ("GER_TEXT20",'Kein B&uuml;ndnis angegeben');
 
-define ("GER_TEXT21",'Bündnis existiert nicht');
+define ("GER_TEXT21",'B&uuml;ndnis existiert nicht');
 
-define ("GER_TEXT22",'Bündnis existiert nicht/wurde bereits angenommen');
+define ("GER_TEXT22",'B&uuml;ndnis existiert nicht/wurde bereits angenommen');
 
-define ("GER_TEXT23",'Bündnis konnte nicht gefunden werden (Wurde das Bündnis eben gerade von der anderen Partei gebrochen?)');
+define ("GER_TEXT23",'B&uuml;ndnis konnte nicht gefunden werden (Wurde das B&uuml;ndnis eben gerade von der anderen Partei gebrochen?)');
 
 define ("GER_TEXT24",'1. Partei konnte nicht gefunden werden');
 
 define ("GER_TEXT25",'2. Partei konnte nicht gefunden werden');
 
-define ("GER_TEXT26",'Bündnis existiert nicht/wurde noch nicht angenommen');
+define ("GER_TEXT26",'B&uuml;ndnis existiert nicht/wurde noch nicht angenommen');
 
-define ("GER_TEXT27",'Bündnis gebrochen');
+define ("GER_TEXT27",'B&uuml;ndnis gebrochen');
 
-define ("GER_TEXT28",'Bündnispartner');
+define ("GER_TEXT28",'B&uuml;ndnispartner');
 
 define ("GER_TEXT29",'besteht seit');
 
@@ -98,5 +98,180 @@ define ("GER_TEXT38",'Ablehnen');
 
 define ("GER_TEXT39",'Vorgeschlagen');
 
-define ("GER_TEXT40",'Zurückziehen');
+define ("GER_TEXT40",'Zur&uuml;ckziehen');
+
+define ("GER_TEXT41",'Wartet');
+
+define ("GER_TEXT42",'Unbekannt');
+
+// English
+define ("ENG_TEXT1",'Diplomacy:');
+
+define ("ENG_TEXT2",'Propose new alliance');
+
+define ("ENG_TEXT3",'Player name:');
+
+define ("ENG_TEXT4",'Search');
+
+define ("ENG_TEXT5",'<< Back');
+
+define ("ENG_TEXT6",'Confirm');
+
+define ("ENG_TEXT7",'Possible names:');
+
+define ("ENG_TEXT8",'Name:');
+
+define ("ENG_TEXT9",'Race:');
+
+define ("ENG_TEXT10",'Alliance:');
+
+define ("ENG_TEXT11",'Points:');
+
+define ("ENG_TEXT12",'Offer alliance');
+
+define ("ENG_TEXT13",'No player name');
+
+define ("ENG_TEXT14",'Allies does not exist');
+
+define ("ENG_TEXT15",'You cannot make an alliance with yourself');
+
+define ("ENG_TEXT16",'You already have an alliance / alliance job with this player');
+
+define ("ENG_TEXT17",'View personal alliance');
+
+define ("ENG_TEXT18",'There was just a new personal alliance. Best review in your user diplomacy.');
+
+define ("ENG_TEXT19",'Propose new alliance');
+
+define ("ENG_TEXT20",'No pact specified');
+
+define ("ENG_TEXT21",'Alliance does not exist');
+
+define ("ENG_TEXT22",'Alliance does not exist / has already been accepted');
+
+define ("ENG_TEXT23",'Alliance could not be found (the alliance has been recently broke from the other party?))');
+
+define ("ENG_TEXT24",'1. Party could not be found');
+
+define ("ENG_TEXT25",'2. Party could not be found');
+
+define ("ENG_TEXT26",'Alliance does not exist / has not been accepted');
+
+define ("ENG_TEXT27",'Alliance broke');
+
+define ("ENG_TEXT28",'Allies');
+
+define ("ENG_TEXT29",'Since');
+
+define ("ENG_TEXT30",'User status');
+
+define ("ENG_TEXT31",'Error: Internal rankings fault!');
+
+define ("ENG_TEXT32",'Please report this to an admin');
+
+define ("ENG_TEXT33",'Break');
+
+define ("ENG_TEXT34",'online');
+
+define ("ENG_TEXT35",'absent');
+
+define ("ENG_TEXT36",'offline');
+
+define ("ENG_TEXT37",'Accept');
+
+define ("ENG_TEXT38",'Decline');
+
+define ("ENG_TEXT39",'Nominated');
+
+define ("ENG_TEXT40",'Retire');
+
+define ("ENG_TEXT41",'Waiting');
+
+define ("ENG_TEXT42",'Unknown');
+
+// Italian
+define ("ITA_TEXT1",'Diplomazia:');
+
+define ("ITA_TEXT2",'Proponi nuova alleanza');
+
+define ("ITA_TEXT3",'Nome giocatore:');
+
+define ("ITA_TEXT4",'Cerca');
+
+define ("ITA_TEXT5",'<< Indietro');
+
+define ("ITA_TEXT6",'Conferma');
+
+define ("ITA_TEXT7",'Nomi possibili:');
+
+define ("ITA_TEXT8",'Nome:');
+
+define ("ITA_TEXT9",'Specie:');
+
+define ("ITA_TEXT10",'Alleanza:');
+
+define ("ITA_TEXT11",'Punti:');
+
+define ("ITA_TEXT12",'Offri alleanza');
+
+define ("ITA_TEXT13",'Nessun nome giocatore');
+
+define ("ITA_TEXT14",'Non esistono alleati');
+
+define ("ITA_TEXT15",'Non puoi creare un alleanza con te stesso');
+
+define ("ITA_TEXT16",'Hai gi&agrave; un alleanza / trattato con questo giocatore');
+
+define ("ITA_TEXT17",'Vedi alleanze personali');
+
+define ("ITA_TEXT18",'&Egrave; appena stata creata una nuova alleanza personale. Maggiori dettagli nella tua sezione diplomatica.');
+
+define ("ITA_TEXT19",'Proponi nuova alleanza');
+
+define ("ITA_TEXT20",'Nessun patto specificato');
+
+define ("ITA_TEXT21",'L\'alleanza non esiste');
+
+define ("ITA_TEXT22",'L\'alleanza non esiste / &egrave; gi&agrave; stata accettata');
+
+define ("ITA_TEXT23",'L\'alleanza non puo essere trovata (l\'alleanza &egrave; stata recentemente rotta dalla controparte?)');
+
+define ("ITA_TEXT24",'1. Parte non può essere trovata');
+
+define ("ITA_TEXT25",'2. Parte non può essere trovata');
+
+define ("ITA_TEXT26",'L\'alleanza non esiste / non &egrave; stata accettata');
+
+define ("ITA_TEXT27",'Alleanza rotta');
+
+define ("ITA_TEXT28",'Alleati');
+
+define ("ITA_TEXT29",'Dal');
+
+define ("ITA_TEXT30",'Stato utente');
+
+define ("ITA_TEXT31",'Errore: errore interno classifiche!');
+
+define ("ITA_TEXT32",'Per favore riportare questo ad un amministratore');
+
+define ("ITA_TEXT33",'Rompere');
+
+define ("ITA_TEXT34",'online');
+
+define ("ITA_TEXT35",'assente');
+
+define ("ITA_TEXT36",'offline');
+
+define ("ITA_TEXT37",'Accetta');
+
+define ("ITA_TEXT38",'Declina');
+
+define ("ITA_TEXT39",'Nominato');
+
+define ("ITA_TEXT40",'Ritira');
+
+define ("ITA_TEXT41",'In attesa');
+
+define ("ITA_TEXT42",'Sconosciuto');
+
 ?>
