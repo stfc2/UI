d6f1f642f94631cd10ef296e5230d7743ec75dfe
merry75 <kirirur@gmail.com>
2008-05-22 16:29:56 +0000

    Aggiunto scheletri file di help in italiano.

---
+++ game/modules/help.php
@@ -0,0 +1,79 @@
+<?PHP
+
+$game->init_player();
+
+$game->out('
+	<center><span class="caption">Aiuto</span>
+	<br><br>
+	<table border=0 cellpadding=2 cellspacing=2 width="95%" class="style_outer" align="center">
+	<tr>
+		<td>
+		<table class="style_inner" width="100%" align="center" border="0" cellpadding="2" cellspacing="5">
+		<tr>
+			<td>
+			<span class="sub_caption">File di aiuto disponibili:</span>
+			</td>
+		</tr>
+		<tr>
+		<td>
+			<a href="JavaScript:void(window.open(\'help/faq.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));"><span class="sub_caption2">FAQ</span></a>
+		</td>
+		</tr>
+		<tr>
+		<td>
+			<a href="JavaScript:void(window.open(\'help/building_1.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Costruzioni</a>
+		</td>
+		</tr>
+		<tr>
+		<td>
+			<a href="JavaScript:void(window.open(\'help/research_catresearch.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Centro Ricerche : Divisione componenti navi</a>
+		</td>
+		</tr>
+		<tr>
+		<td>
+			<a href="JavaScript:void(window.open(\'help/research_localresearch.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Centro Ricerche : Divisione ricerche planetarie</a>
+		</td>
+		</tr>
+		<tr>
+		<td>
+			<a href="JavaScript:void(window.open(\'help/shipyard_1.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Cantiere Navale</a>
+		</td>
+		</tr>
+		<tr>
+		<td>
+			<a href="JavaScript:void(window.open(\'help/shipyard_2.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Cantiere Navale : Equipaggio navi</a>
+		</td>
+		</tr>
+		<tr>
+		<td>
+			<a href="JavaScript:void(window.open(\'help/shipyard_3.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Cantiere Navale : Stato costruzione</a>
+		</td>
+		</tr>
+		<tr>
+		<td>
+			<a href="JavaScript:void(window.open(\'help/academy_1.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Accademia : Stato accademia</a>
+		</td>
+		</tr>
+		<tr>
+		<td>
+			<a href="JavaScript:void(window.open(\'help/academy_2.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Accademia : Lista addestramento</a>
+		</td>
+		</tr>
+		<tr>
+		<td>
+			<a href="JavaScript:void(window.open(\'help/planetlist.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Pianeti</a>
+		</td>
+		</tr>
+		<tr>
+		<td>
+			<a href="JavaScript:void(window.open(\'help/ships.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Navi</a>
+		</td>
+		</tr>
+		</table>
+		</td>
+	</tr>
+	</table>
+	</center>
+	');
+
+?>
