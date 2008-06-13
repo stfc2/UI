9de7a7f43fa653ea3abdc64f20796c115117cbde
merry75 <kirirur@gmail.com>
2008-06-13 18:42:46 +0000

    Compattato e ripulito il file.

--- game/include/sql.php
+++ game/include/sql.php
@@ -24,479 +24,231 @@
 class sql {
 
     var $login = array(
-
         'server' => 'localhost',
-
         'database' => 'mysql',
-
         'user' => 'root',
-
         'password' => '',
-
     );
-
-    
 
     var $error = array(
-
         'message' => 'No error occured',
-
         'number' => '0',
-
         'sql' => ''
-
     );
 
-
-
     var $link_id = 0;
-
     var $query_id = 0;
 
-
-
     var $i_query = 0;
-
     var $t_query = 0;
-
 
 
     var $debug = false;
 
     var $d_query = array();
 
-
-
-
-
     function sql($server, $database, $user, $password = '') {
-
         $this->login = array(
-
             'server' => $server,
-
             'database' => $database,
-
             'user' => $user,
-
             'password' => $password
-
         );
-
     }
-
-
 
     function raise_error($message = false, $number = false, $sql = '') {
-
         if($message === false) $message = mysql_error($this->link_id);
-
         if($number === false) $number = mysql_errno($this->link_id);
 
-
-
         $this->error = array(
-
             'message' => $message,
-
             'number' => $number,
-
             'sql' => $sql
-
         );
 
-
-
         return false;
-
     }
-
-
 
     function connect() {
-
         if(!is_resource($this->link_id)) {
-
             if(!$this->link_id = @mysql_connect($this->login['server'], $this->login['user'], $this->login['password'])) {
-
                 message(GENERAL, 'Could not connect to mysql server');
-
             }
-
-
 
             if(!@mysql_select_db($this->login['database'], $this->link_id)) {
-
                 $this->raise_error(false, false, 'USE '.$this->login['database']);
 
-
-
                 message(DATABASE_ERROR, 'Could not select database');
-
             }
-
         }
 
-
-
         return true;
-
     }
-
-
 
     function close() {
-
         if(is_resource($this->link_id)) {
-
             if(!@mysql_close($this->link_id)) {
-
                 return $this->raise_error();
-
             }
-
         }
 
-
-
         return true;
-
     }
-
-    
 
     function lock() {
-
         $tables = func_get_args();
-
-        
-
         $tables[] = 'planets';
-
         $tables[] = 'starsystems';
-
         $tables[] = 'user';
-
-        
-
         $n_tables = count($tables);
-
         $table_str = array();
-
-        
-
         if($n_tables == 0) return;
-
-        
-
         for($i = 0; $i < $n_tables; ++$i) {
-
             $table_str[] = $tables[$i].' WRITE';
-
         }
-
-        
-
         $sql = 'LOCK TABLES '.implode(',', $table_str);
-
-        
-
         if(!mysql_query($sql, $this->link_id)) {
-
             $this->raise_error(false, false, $sql);
-
-            
-
             message(DATABASE_ERROR, 'Could not lock tables');
-
         }
-
-        
-
         return true;
-
     }
-
-    
 
     function unlock() {
-
         if(!mysql_query('UNLOCK TABLES', $this->link_id)) {
-
             $this->raise_error(false, false, 'UNLOCK TABLES');
-
-            
-
             message(DATABASE_ERROR, 'Could not unlock tables');
-
         }
-
-        
-
         return true;
-
     }
-
-    
 
     function query_lowlevel($sql) {
-
         if(!$this->connect()) return false;
-
-        
 
         return @mysql_query($sql, $this->link_id);
-
     }
 
-
-
     function query($query, $unbuffered = false) {
-    	global $game;
-
         if(!$this->connect()) return false;
-/*
-		if (!(strpos($query,"UPDATE planets")===FALSE) && (!(strpos($query,"unit_1")===FALSE) || !(strpos($query,"unit_2")===FALSE) || !(strpos($query,"unit_3")===FALSE)))
-		{
-		$fp = fopen("unitqueries.htm", 'a');
-        fwrite($fp, '<b>['.date('d.m.y H:i:s', time()).']: '.$game->player['user_name'].' ('.$_REQUEST['a'].') -> auf Planet '.$game->planet['planet_name'].' </b><br>'.$query.'<br><br><br>');
-        fclose($fp);	
-		}
 
-*/
         $query_function = ($unbuffered) ? 'mysql_unbuffered_query' : 'mysql_query';
-
-
 
         $start_time = time() + microtime();
 
-
-
         if(!$this->query_id = @$query_function($query, $this->link_id)) {
-
             return $this->raise_error(false, false, $query);
-
         }
-
-
 
         $total_time = (time() + microtime()) - $start_time;
 
-
-
         if($this->debug) {
-
             $this->d_query[] = array(
-
                 'sql' => $query,
-
                 'time' => $total_time
-
             );
-
         }
 
-
-
         $this->t_query += $total_time;
-
         ++$this->i_query;
 
-
-
         return $this->query_id;
-
     }
 
 
 
     function fetchrow($query_id = false, $result_type = MYSQL_ASSOC) {
-
         if($query_id === false) $query_id = $this->query_id;
-
-
 
         if(!$_row = @mysql_fetch_array($query_id, $result_type)) {
-
             if(($_error = mysql_error()) !== '') {
-
                 return $this->raise_error($_error);
-
             }
-
             else {
-
                 return array();
-
             }
-
         }
 
-
-
         return $_row;
-
     }
 
-
-
     function fetchrowset($query_id = false, $result_type = MYSQL_ASSOC) {
-
         if($query_id === false) $query_id = $this->query_id;
-
-
 
         $_row = $_rowset = array();
 
-
-
         while($_row = @mysql_fetch_array($query_id, $result_type)) {
-
             $_rowset[] = $_row;
-
         }
-
-
 
         if(!$_rowset) {
-
             if(($_error = mysql_error()) !== '') {
-
                 return $this->raise_error();
-
             }
-
             else {
-
                 return array();
-
             }
-
         }
-
-
 
         return $_rowset;
-
     }
-
-
 
     function queryrow($query, $result_type = MYSQL_ASSOC) {
-
         if(!$_qid = $this->query($query)) {
-
             return false;
-
         }
-
-
 
         return $this->fetchrow($_qid, $result_type);
-
     }
-
-
 
     function queryrowset($query, $result_type = MYSQL_ASSOC) {
-
         if(!$_qid = $this->query($query, true)) {
-
             return false;
-
         }
-
-
 
         return $this->fetchrowset($_qid, $result_type);
-
     }
-
-
 
     function free_result($query_id = false) {
-
         if($query_id === false) $query_id = $this->query_id;
-
-
 
         if(!@mysql_free_result($query_id)) {
-
             return $this->raise_error();
-
         }
 
-
-
         return true;
-
     }
 
-
-
     function num_rows($query_id = false) {
-
         if($query_id === false) $query_id = $this->query_id;
-
-
 
         $_num = @mysql_num_rows($query_id);
 
-
-
         if($_num === false) {
-
             return $this->raise_error();
-
         }
 
-
-
         return $_num;
-
     }
-
-
 
     function affected_rows() {
-
         $_num = @mysql_affected_rows($this->link_id);
 
-
-
         if($_num === false) {
-
             return $this->raise_error();
-
         }
-
-
 
         return $_num;
-
     }
-
-
 
     function insert_id() {
-
         $_id = @mysql_insert_id($this->link_id);
 
-
-
         if($_id === false) {
-
             return $this->raise_error();
-
         }
 
-
-
         return $_id;
-
     }
-
-    
 
     function debug_info() {
 
