9fc4ec27c043817f4489088fa5806be7978f82e1
kail72 <cristiano.delogu.72@gmail.com>
2008-06-15 22:38:30 +0000

    Versione BETA, con la modifica per l'assegnazione di un sistema solare ad un nuovo giocatore.

--- game/include/functions.php
+++ game/include/functions.php
@@ -1688,7 +1688,16 @@ echo'
 
 			$db->lock('starsystems_slots');
 //			$planet_id = $this->create_planet($this->player['user_id'], 'quadrant', mt_rand(1, 4)); creazione del pianeta madre in maniera randomica
-			$planet_id = $this->create_planet($this->player['user_id'], 'quadrant', $this->pick_quadrant());
+//			$planet_id = $this->create_planet($this->player['user_id'], 'quadrant', $this->pick_quadrant());
+			// Diamo un sistema nuovo al giocatore nuovo
+			$_temp = $this->create_system('quadrant', $this->pick_quadrant());
+            $_system_id = $_temp[0];
+			$_temp = $this->create_planet(0, 'system', $_system_id);
+			$_temp = $this->create_planet(0, 'system', $_system_id);
+			$_temp = $this->create_planet(0, 'system', $_system_id);
+			$_temp = $this->create_planet(0, 'system', $_system_id);
+			$planet_id = $this->create_planet($this->player['user_id'], 'system', $_system_id);
+					
 			$db->unlock();
 
 			if(empty($planet_id)) {
