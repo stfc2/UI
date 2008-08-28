9cf14d5b0492f3454e95aac05bd4459c6de4cd0e
merry75 <kirirur@gmail.com>
2008-08-28 10:05:23 +0000

    La mappa del giocatore nel caso in cui venga visualizzata da un altro giocatore esterno all'alleanza, ora viene creata solo con i sistemi conosciuti.

--- game/usermap.php
+++ game/usermap.php
@@ -20,38 +20,53 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+error_reporting(E_ALL);
 
 // include global definitions + functions + game-class + player-class
 include_once('include/global.php');
 include_once('include/sql.php');
 include_once('include/functions.php');
-error_reporting(E_ALL);
 
 
 // create sql-object for db-connection
 $db = new sql($config['server'].":".$config['port'], $config['game_database'], $config['user'], $config['password']); // create sql-object for db-connection
+
+// Load player parameters 
+$game = new game();
+
+$game->load_config();
+$ACTUAL_TICK = $NEXT_TICK = 0;
+
+$ACTUAL_TICK = $game->config['tick_id'];
+$NEXT_TICK = $game->config['tick_time'] - time();//$game->TIME;
+$LAST_TICK_TIME = $game->config['tick_time'] - TICK_DURATION * 60;
+
+include('include/session.php');
+// load player parameters
+
+// Check map size request
 if ($_GET['size']<1) $_GET['size']=1;
 if ($_GET['size']>8) $_GET['size']=8;
 
 $size=$_GET['size'];
 
-
-$sql = 'SELECT user_id FROM user WHERE user_name LIKE "'.addslashes($_GET['user']).'"';
+// Read user ID from DB
+$sql = 'SELECT user_id, user_alliance FROM user WHERE user_name LIKE "'.addslashes($_GET['user']).'"';
 $user = $db->queryrow($sql);
-  if(!isset($user['user_id'])) {
-  	
-		/*	header("Content-type: image/png");
-			header("Pragma: no-cache");
-			imagepng($im);*/
-			exit;
-       }
-       
-       
-       
+if(!isset($user['user_id'])) {
+
+	/*header("Content-type: image/png");
+	header("Pragma: no-cache");
+	imagepng($im);*/
+	exit;
+}
+
+
+
 //Assign names:
-$image_url='maps/tmp/u_'.$user['user_id'].'_'.$size.'.png';
-$map_url='maps/tmp/u_'.$user['user_id'].'_'.$size.'.html';
-       
+$image_url='maps/tmp/u_'.$user['user_id'].'_'.$game->player['user_id'].'_'.$size.'.png';
+$map_url='maps/tmp/u_'.$user['user_id'].'_'.$game->player['user_id'].'_'.$size.'.html';
+
 
 
 
@@ -65,15 +80,31 @@ if (file_exists($image_url))
 	}
 }
 
+// Localize strings
+switch($game->player['language'])
+{
+	case 'GER':
+		$title = 'Karte von Spieler '.$_GET['user'].':';
+		$created = 'Erstellt der ';
+	break;
+	case 'ITA':
+		$title = 'Mappa del giocatore '.$_GET['user'].':';
+		$created = 'Creata il ';
+	break;
+	default:
+		$title = 'Galaxy map of player '.$_GET['user'].':';
+		$created = 'Created at ';
+	break;
+}
 
 
 
 // Generates new pictures?: 
 if (($handle = @fopen ($image_url, "rb"))!=true)
 {
-	
+
 $map_data='<map name="detail_map">';
-	
+
 $im = imagecreatetruecolor(162*$size, 162*$size);
 imagecolorallocatealpha($im, 0, 0, 0,0);
 $color[1]=imagecolorallocatealpha($im, 90, 64, 64,0);
@@ -110,9 +141,18 @@ SELECT s.system_id, s.system_name, s.sector_id, s.system_x, s.system_y
 while($system = $db->fetchrow($q_systems))
 $glob_systems[$system['system_id']]=$system;
 
+if($game->player['user_id'] == $user['user_id'] || $game->player['user_alliance'] == $user['user_alliance']) {
+    $sql = 'SELECT system_id FROM planets WHERE planet_owner='.$user['user_id'].' GROUP BY system_id';
+}
+else {
+    $sql = 'SELECT pl.system_id FROM (planets pl)
+                   LEFT JOIN (planet_details pd) on pl.system_id = pd.system_id
+            WHERE pl.planet_owner = '.$user['user_id'].' AND
+                  pd.user_id = "'.$game->player['user_id'].'" AND pd.log_code = 500
+            GROUP BY pl.system_id';
+}
 
-
-$q_planets = $db->query('SELECT system_id FROM planets WHERE planet_owner='.$user['user_id'].' GROUP BY system_id');
+$q_planets = $db->query($sql);
 
 while($planet = $db->fetchrow($q_planets)) {
 $system=$glob_systems[$planet['system_id']];
@@ -188,11 +228,11 @@ else $size2=$size-2;
 
 if ($size>1)
 {
-imagestring ($im, $size2,15,162*$size-12-$size,'Created at '.date('d.m.y H:i', time()), $color[4]);
+imagestring ($im, $size2,15,162*$size-12-$size,$created.date('d.m.y H:i', time()), $color[4]);
 }
 else
 {
-imagestring ($im, $size2,5,162*$size-15,'Created at', $color[4]);
+imagestring ($im, $size2,5,162*$size-15,$created, $color[4]);
 imagestring ($im, $size2,5,162*$size-8,date('d.m.y H:i', time()), $color[4]);
 }
 
@@ -242,17 +282,17 @@ fclose($handle);
 }
 
 }
-	
+
 echo'<html><body bgcolor="#000000" text="#DDDDDD"  background="'.$config['game_url'].'/gfx/bg_stars1.gif"><center>
-<span style="font-family: Verdana; font-size: 15px;"><b>Galaxy map of player '.$_GET['user'].':</b></span><br>
+<span style="font-family: Verdana; font-size: 15px;"><b>'.$title.'</b></span><br>
 <img border=0 usemap="#detail_map" src="'.$image_url.'" >
 
 '.$map_data.'
 
 </body></html>';
 
-	
-	
+
+
 }
 
 $db->close();
