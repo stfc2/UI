5182e27e1f594a989fca882461817f2ff3a744fc
merry75 <kirirur@gmail.com>
2008-10-06 08:54:52 +0000

    Aggiunte pagine per la gestione delle donazioni.

---
+++ pages/spende.php
@@ -0,0 +1,58 @@
+<?PHP
+/*    
+	This file is part of STFC.
+	Copyright 2006-2007 by Michael Krauss (info@stfc2.de) and Tobias Gafner
+		
+	STFC is based on STGC,
+	Copyright 2003-2007 by Florian Brede (florian_brede@hotmail.com) and Philipp Schmidt
+	
+    STFC is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    STFC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+$main_html='<center><span class="caption">Supporta Star Trek: Frontline Combat!</span>
+<br><br>
+<table border=0 cellpadding=0 cellspacing=0>
+<tr><td width=300 valign=top align="justify">
+<p>Cari colleghi e giocatori,<br><br>
+a seguito di alcune defezioni nello staff tecnico che da a tutti la possibilit&agrave; di divertirci con <b>Star Trek Frontline Combat</b>,
+le spese di gestione del server stanno diventando poco gestibili.</p>
+<p>Tenteremo per quanto possibile di sostenere la situazione, tuttavia il rischio di chiusura del server &egrave; tristemente concreto.</p>
+<p>Dunque ogni contributo spontaneo &egrave; assolutamente benaccetto! Visto il numero di utenti, basterebbe davvero poco da ciascuno per
+scongiurare il rischio di chiusura: pensate solo a quanto costano normalmente i videogiochi ed eventuali abbonamenti per giocare online,
+credo che di fronte a queste cifre diventi accettabile "<i>offrir da bere</i>" a chi senza aver mai chiesto nulla si &egrave; sempre prodigato a mantenere attivo il sito per il vostro divertimento.<p>
+<p>Purtroppo non sempre si pu&ograve; solo donare, per quanto bello, a volte ci si trova nelle condizioni di dover cortesemente chiedere... sperando di avere risposte positive dalle persone presenti dietro ai giocatori che si &egrave; iniziato a stimare nel nostro piccolo spazio virtuale.</p>
+<p>Se la generosit&agrave; di tutti dovesse superare le aspettative, ovviamente un eventuale eccesso verrebbe reinvestito nel potenziamento
+del server, oppure nel pagamento degli anni successivi.</p>
+<p>Grazie a tutti e buon gioco!</p>
+</td>
+<td width="220" valign="middle" align="center">
+<img src="gfx/uncle-sam-wants-you.jpg" width="170">
+</td>
+</tr>
+<tr>
+<td colspan="2" align="center">
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="170063">
+<input type="image" src="http://www.stfc.it/gfx/paypal.gif" border="0" name="submit" alt="">
+<img alt="" border="0" src="https://www.paypal.com/it_IT/i/scr/pixel.gif" width="1" height="1">
+</form>
+</td>
+</tr></table>
+</center>
+
+
+';
+?>
---
+++ pages/success.php
@@ -0,0 +1,38 @@
+<?PHP
+/*    
+	This file is part of STFC.
+	Copyright 2006-2007 by Michael Krauss (info@stfc2.de) and Tobias Gafner
+		
+	STFC is based on STGC,
+	Copyright 2003-2007 by Florian Brede (florian_brede@hotmail.com) and Philipp Schmidt
+	
+    STFC is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    STFC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+$main_html='<center><span class="caption">Grazie per la tua donazione!</span>
+<br><br>
+<table border=0 cellpadding=0 cellspacing=0>
+<tr><td width=300 valign=top align="justify">
+<p>Grazie per aver effettuato la tua donazione.</p>
+<p>La transazione &egrave; stata completata e una ricevuta dell&#146;acquisto &egrave; stata inviata al tuo indirizzo email.</p>
+<p>Per visualizzare i dettagli sulla transazione effettua l&#146;accesso al tuo conto dall&#146;indirizzo 
+<a href="http://www.paypal.com/it">www.paypal.com/it</a>.</p>
+</td>
+</tr></table>
+</center>
+
+
+';
+?>
