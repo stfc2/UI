35929ba9ed07705e250fd87d9e7e05c88a39a5f9
merry75 <kirirur@gmail.com>
2008-10-06 08:34:17 +0000

    Corretto l'url tapullo del forum alleanza.

--- game/include/functions.php
+++ game/include/functions.php
@@ -1065,7 +1065,7 @@ class game {
 		{
 			// 21/02/08 - AC: Check if alliance homepage is empty
 			if($alliance_hp['hp'] == '')
-				$alliance_hp['hp'] = parse_link('a=alliance_board'); // <- temp solution!!
+				$alliance_hp['hp'] = JSCRIPT_PATH.parse_link('a=alliance_board'); // <- temp solution!!
 			$pos1 = stripos($alliance_hp['hp'], "http://");
 			if($pos1!==false)
 			{
