82c28f0ab0ec395557f4f813499ddf64725531af
merry75 <kirirur@gmail.com>
2008-10-24 10:00:12 +0000

    Localizzate in tedesco ed inglese le stringhe della chat alleanza.

--- game/modules/alliance_chat.sprache.php
+++ game/modules/alliance_chat.sprache.php
@@ -22,33 +22,33 @@
 */
 
 //Deutsch
-define ("GER_TEXT0", 'Non hai i permessi necessari per accedere a questa funzione!!!!');
+define ("GER_TEXT0", 'Sie haben keine Berechtigung f&uuml;r den Zugriff auf diese Funktion!');
 
-define ("GER_TEXT1", 'Du darfst nur alle 15 Sekunden in der Shoutbox etwas posten.');
+define ("GER_TEXT1", 'Du darfst nur alle 5 Sekunden in der Shoutbox etwas posten.');
 
 define ("GER_TEXT2", 'Allianz Shoutbox:');
 
 define ("GER_TEXT3", 'Senden');
 
-define ("GER_TEXT4", 'N.B. Per visualizzare i nuovi messaggi di altri utenti, usare la funzione "Ricarica Pagina Corrente" del proprio browser!');
+define ("GER_TEXT4", 'Hinweis: Um neue Nachrichten von anderen Benutzern, m&uuml;ssen Sie die "aktuelle Seite neu laden"-Funktion Ihres Browsers!');
 
 
 //English
-define ("ENG_TEXT0", 'Non hai i permessi necessari per accedere a questa funzione!!!!');
+define ("ENG_TEXT0", 'You do not have permission to access this feature!');
 
-define ("ENG_TEXT1", 'You can post something in the Shoutbox only every 15 seconds.');
+define ("ENG_TEXT1", 'You can post something in the Shoutbox only every 5 seconds.');
 
 define ("ENG_TEXT2", 'Alliance Shoutbox:');
 
 define ("ENG_TEXT3", 'Send');
 
-define ("ENG_TEXT4", 'N.B. Per visualizzare i nuovi messaggi di altri utenti, usare la funzione "Ricarica Pagina Corrente" del proprio browser!');
+define ("ENG_TEXT4", 'NB In order to view new messages from other users, you must use the "Reload current page" function of your browser!');
 
 
 //Italian
-define ("ITA_TEXT0", 'Non hai i permessi necessari per accedere a questa funzione!!!!');
+define ("ITA_TEXT0", 'Non hai i permessi necessari per accedere a questa funzione!');
 
-define ("ITA_TEXT1", 'Attendi 15 secondi prima di inviare un nuovo messaggio!!!');
+define ("ITA_TEXT1", 'Devi attendere 5 secondi prima di inviare un nuovo messaggio!');
 
 define ("ITA_TEXT2", 'Shoutbox di Alleanza:');
 
