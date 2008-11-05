9aa6603d2efd12355ec4de9cf1a77999829b750a
merry75 <kirirur@gmail.com>
2008-11-05 16:14:32 +0000

    Localizzato modulo help.

--- game/modules/help.php
+++ game/modules/help.php
@@ -3,7 +3,7 @@
 $game->init_player();
 
 $game->out('
-	<span class="caption">Aiuto</span>
+	<span class="caption">'.constant($game->sprache("TEXT0")).'</span>
 	<br><br>
 	<table border=0 cellpadding=2 cellspacing=2 width="600" class="style_outer" align="center">
 	<tr>
@@ -11,84 +11,84 @@ $game->out('
 		<table class="style_inner" width="300" align="center" border="0" cellpadding="2" cellspacing="5">
 		<tr>
 			<td>
-			<span class="sub_caption">File di aiuto disponibili: *</span>
+			<span class="sub_caption">'.constant($game->sprache("TEXT100")).'</span>
 			</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/faq.htm\',\'STFC\',\'toolbar=no,width=750,height=600,resizable=no,scrollbars=yes\'));"><span class="sub_caption2">FAQ</span></a>
+			<a href="JavaScript:void(window.open(\'help/faq.htm\',\'STFC\',\'toolbar=no,width=750,height=600,resizable=no,scrollbars=yes\'));"><span class="sub_caption2">'.constant($game->sprache("TEXT101")).'</span></a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/building_1.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Costruzioni</a>
+			<a href="JavaScript:void(window.open(\'help/building_1.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT102")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/research_catresearch.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Centro Ricerche : Divisione componenti navi</a>
+			<a href="JavaScript:void(window.open(\'help/research_catresearch.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT103")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/research_localresearch.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Centro Ricerche : Divisione ricerche planetarie</a>
+			<a href="JavaScript:void(window.open(\'help/research_localresearch.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT104")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/shipyard_1.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Cantiere Navale</a>
+			<a href="JavaScript:void(window.open(\'help/shipyard_1.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT105")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/shipyard_2.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));"><s>Cantiere Navale : Equipaggio navi</s></a>
+			<a href="JavaScript:void(window.open(\'help/shipyard_2.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT106")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/shipyard_3.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));"><s>Cantiere Navale : Stato costruzione</s></a>
+			<a href="JavaScript:void(window.open(\'help/shipyard_3.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT107")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/academy_1.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Accademia : Stato accademia</a>
+			<a href="JavaScript:void(window.open(\'help/academy_1.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT108")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/academy_2.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Accademia : Lista addestramento</a>
+			<a href="JavaScript:void(window.open(\'help/academy_2.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT109")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/trade_viewstatus.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));"><s>Centro commerciale : Stato consegne</s></a>
+			<a href="JavaScript:void(window.open(\'help/trade_viewstatus.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT110")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/trade_buy_truppen.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));"><s>Centro commerciale : Acquisto truppe</s></a>
+			<a href="JavaScript:void(window.open(\'help/trade_buy_truppen.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT111")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/trade_sold_truppen.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));"><s>Centro commerciale : Vendita truppe</s></a>
+			<a href="JavaScript:void(window.open(\'help/trade_sold_truppen.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT112")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/trade_ress.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));"><s>Centro commerciale : Commercio risorse</s></a>
+			<a href="JavaScript:void(window.open(\'help/trade_ress.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT113")).'</a>
 		</td>
 		</tr>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/planetlist.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Pianeti</a>
+			<a href="JavaScript:void(window.open(\'help/planetlist.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT120")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="JavaScript:void(window.open(\'help/ships.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">Navi</a>
+			<a href="JavaScript:void(window.open(\'help/ships.htm\',\'STFC\',\'toolbar=no,width=600,height=400,resizable=no,scrollbars=yes\'));">'.constant($game->sprache("TEXT121")).'</a>
 		</td>
 		</tr>
 		</table>
@@ -97,32 +97,32 @@ $game->out('
 		<table class="style_inner" width="300" align="center" border="0" cellpadding="2" cellspacing="5">
 		<tr>
 			<td>
-			<span class="sub_caption">Guida in game:</span>
+			<span class="sub_caption">'.constant($game->sprache("TEXT200")).'</span>
 			</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="'.parse_link('a=database&view=guide').'"><span class="sub_caption2">INDICE</span></a>
+			<a href="'.parse_link('a=database&view=guide').'"><span class="sub_caption2">'.constant($game->sprache("TEXT201")).'</span></a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="'.parse_link('a=database&view=guide&page=1').'">Il gioco</a>
+			<a href="'.parse_link('a=database&view=guide&page=1').'">'.constant($game->sprache("TEXT202")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="'.parse_link('a=database&view=guide&page=2').'">I pianeti</a>
+			<a href="'.parse_link('a=database&view=guide&page=2').'">'.constant($game->sprache("TEXT203")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="'.parse_link('a=database&view=guide&page=3').'">Le strutture</a>
+			<a href="'.parse_link('a=database&view=guide&page=3').'">'.constant($game->sprache("TEXT204")).'</a>
 		</td>
 		</tr>
 		<tr>
 		<td>
-			<a href="'.parse_link('a=database&view=guide&page=4').'">Le ricerche planetarie</a>
+			<a href="'.parse_link('a=database&view=guide&page=4').'">'.constant($game->sprache("TEXT205")).'</a>
 		</td>
 		</tr>
 		<tr>
@@ -135,12 +135,12 @@ $game->out('
 	</tr>
 	<tr>
 		<td align="center" colspan="2">
-		<a href="'.parse_link('a=messages&a2=newpost&receiver=STFC-Support&subject=Richiesta%20di%20aiuto').'"><span style="color: #FFFF00; font-weight: bold; font-size: 11pt;"><u>Se ti senti perduto, segui questo link per scrivere al Supporto!</u></a></span>
+		<a href="'.parse_link('a=messages&a2=newpost&receiver=STFC-Support&subject='.constant($game->sprache("TEXT1"))).'"><span style="color: #FFFF00; font-weight: bold; font-size: 11pt;"><u>'.constant($game->sprache("TEXT2")).'</u></a></span>
 		</td>
 	</tr>
 	<tr>
 	<td colspan="2">
-	<br><br>* I file di aiuto contrassegnati dal nome <s>barrato</s> non sono al momento disponibili.
+	<br><br>'.constant($game->sprache("TEXT3")).'
 	</td>
 	</tr>
 	</table>
---
+++ game/modules/help.sprache.php
@@ -0,0 +1,115 @@
+<?PHP
+
+//Deutsch
+define ("GER_TEXT0", 'Hilfe');
+define ("GER_TEXT1", 'Anfrage%20f&uuml;r%20Hilfe');
+define ("GER_TEXT2", 'Wenn Sie der Meinung sind verloren, folgen Sie diesem Link zu schreiben, um den Support!');
+define ("GER_TEXT3", '* Die Hilfe-Dateien mit dem Namen <s>durchgestrichen</s> sind zurzeit nicht verf&uuml;gbar.');
+
+define ("GER_TEXT100", 'Verf&uuml;gbare Hilfe-Dateien: *');
+define ("GER_TEXT101", 'FAQ');
+define ("GER_TEXT102", 'Geb&auml;ude');
+define ("GER_TEXT103", 'Forschungszentrum : Schiffskomponentenabteilung');
+define ("GER_TEXT104", 'Forschungszentrum : Planetare Forschungsabteilung');
+define ("GER_TEXT105", 'Schiffswerft');
+define ("GER_TEXT106", 'Schiffswerft : Besatzung f&uuml;r Schiffe');
+define ("GER_TEXT107", 'Schiffswerft : Baustatus');
+define ("GER_TEXT108", 'Akademie : Ausbildungsstatus');
+define ("GER_TEXT109", 'Akademie : Ausbildungsliste');
+define ("GER_TEXT110", '<s>Handelszentrum : Lieferstatus einsehen</s>');
+define ("GER_TEXT111", '<s>Handelszentrum : Truppen Kauf</s>');
+define ("GER_TEXT112", '<s>Handelszentrum : Truppen Verkauf</s>');
+define ("GER_TEXT113", '<s>Handelszentrum : Ressourcenmarkt');
+define ("GER_TEXT114", '<s>Handelszentrum : Auktion erstellen</s>');
+define ("GER_TEXT115", '<s>Handelszentrum : Auktionen einsehen</s>');
+define ("GER_TEXT116", '<s>Handelszentrum : Eigene auktionen</s>');
+define ("GER_TEXT117", '<s>Handelszentrum : Konto Auszahlung</s>');
+define ("GER_TEXT118", '<s>Handelszentrum : Auktionsschulden</s>');
+define ("GER_TEXT119", '<s>Handelszentrum : Schiffabhohl Stelle</s>');
+define ("GER_TEXT120", 'Planeten');
+define ("GER_TEXT121", 'Schiffe');
+
+define ("GER_TEXT200", 'Ingame guide:');
+define ("GER_TEXT201", 'INDEX');
+define ("GER_TEXT202", 'The game');
+define ("GER_TEXT203", 'The planets');
+define ("GER_TEXT204", 'The structures');
+define ("GER_TEXT205", 'The planetary researches');
+
+
+// English
+define ("ENG_TEXT0", 'Help');
+define ("ENG_TEXT1", 'Request%20for%20help');
+define ("ENG_TEXT2", 'If you feel lost, follow this link to write to the Support!');
+define ("ENG_TEXT3", '* The help files marked with the <s>name crossed out</s> are not currently available.');
+
+define ("ENG_TEXT100", 'Available help files: *');
+define ("ENG_TEXT101", 'FAQ');
+define ("ENG_TEXT102", 'Buildings');
+define ("ENG_TEXT103", 'Research center : Ship Components Division');
+define ("ENG_TEXT104", 'Research center : Planetary Research Division');
+define ("ENG_TEXT105", 'Shipyard');
+define ("ENG_TEXT106", 'Shipyard : Ships crew');
+define ("ENG_TEXT107", 'Shipyard : Building status');
+define ("ENG_TEXT108", 'Accademy : Training status');
+define ("ENG_TEXT109", 'Accademy : Training list');
+define ("ENG_TEXT110", '<s>Trade center : Delivery status</s>');
+define ("ENG_TEXT111", '<s>Trade center : Troops purchasing</s>');
+define ("ENG_TEXT112", '<s>Trade center : Troops sales</s>');
+define ("ENG_TEXT113", '<s>Trade center : Resources market');
+define ("ENG_TEXT114", '<s>Trade center : Creating auction</s>');
+define ("ENG_TEXT115", '<s>Trade center : View auctions</s>');
+define ("ENG_TEXT116", '<s>Trade center : Own auctions</s>');
+define ("ENG_TEXT117", '<s>Trade center : Trust account</s>');
+define ("ENG_TEXT118", '<s>Trade center : Pay debts</s>');
+define ("ENG_TEXT119", '<s>Trade center : Ships withdrawal</s>');
+define ("ENG_TEXT120", 'Pianets');
+define ("ENG_TEXT121", 'Ships');
+
+define ("ENG_TEXT200", 'Ingame guide:');
+define ("ENG_TEXT201", 'INDEX');
+define ("ENG_TEXT202", 'The game');
+define ("ENG_TEXT203", 'The planets');
+define ("ENG_TEXT204", 'The structures');
+define ("ENG_TEXT205", 'The planetary researches');
+
+
+//Italiano
+define ("ITA_TEXT0", 'Aiuto');
+define ("ITA_TEXT1", 'Richiesta%20di%20aiuto');
+define ("ITA_TEXT2", 'Se ti senti perduto, segui questo link per scrivere al Supporto!');
+define ("ITA_TEXT3", '* I file di aiuto contrassegnati dal nome <s>barrato</s> non sono al momento disponibili.');
+
+define ("ITA_TEXT100", 'File di aiuto disponibili: *');
+define ("ITA_TEXT101", 'FAQ');
+define ("ITA_TEXT102", 'Costruzioni');
+define ("ITA_TEXT103", 'Centro Ricerche : Divisione componenti navi');
+define ("ITA_TEXT104", 'Centro Ricerche : Divisione ricerche planetarie');
+define ("ITA_TEXT105", 'Cantiere Navale');
+define ("ITA_TEXT106", 'Cantiere Navale : Equipaggio navi');
+define ("ITA_TEXT107", 'Cantiere Navale : Stato costruzione');
+define ("ITA_TEXT108", 'Accademia : Stato accademia');
+define ("ITA_TEXT109", 'Accademia : Lista addestramento');
+define ("ITA_TEXT110", '<s>Centro commerciale : Stato consegne</s>');
+define ("ITA_TEXT111", '<s>Centro commerciale : Acquisto truppe</s>');
+define ("ITA_TEXT112", '<s>Centro commerciale : Vendita truppe</s>');
+define ("ITA_TEXT113", '<s>Centro commerciale : Commercio risorse');
+define ("ITA_TEXT114", '<s>Centro commerciale : Creazione asta</s>');
+define ("ITA_TEXT115", '<s>Centro commerciale : Visualizzazione aste</s>');
+define ("ITA_TEXT116", '<s>Centro commerciale : Aste proprie</s>');
+define ("ITA_TEXT117", '<s>Centro commerciale : Conto fiduciario</s>');
+define ("ITA_TEXT118", '<s>Centro commerciale : Pagamento debiti</s>');
+define ("ITA_TEXT119", '<s>Centro commerciale : Ritiro navi</s>');
+define ("ITA_TEXT120", 'Pianeti');
+define ("ITA_TEXT121", 'Navi');
+
+define ("ITA_TEXT200", 'Guida in game:');
+define ("ITA_TEXT201", 'INDICE');
+define ("ITA_TEXT202", 'Il gioco');
+define ("ITA_TEXT203", 'I pianeti');
+define ("ITA_TEXT204", 'Le strutture');
+define ("ITA_TEXT205", 'Le ricerche planetarie');
+
+
+
+?>
