11f0b2b803a642d4a91030ae96dd5283df358277
merry75 <kirirur@gmail.com>
2013-03-11 13:58:05 +0000

    Added a new admin tool to reset the galaxy.
    TODO:
    - remove some default values in admin user creation
    - automatically install BOTS

---
+++ game/modules/tools/world/reset_galaxy.php
@@ -0,0 +1,300 @@
+<?php
+/*    
+	This file is part of STFC.
+	Copyright 2006-2007 by Michael Krauss (info@stfc2.de) and Tobias Gafner
+		
+	STFC is based on STGC,
+	Copyright 2003-2007 by Florian Brede (florian_brede@hotmail.com) and Philipp Schmidt
+	
+    STFC is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    STFC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+$game->init_player();
+
+$game->out('<span class="caption">Reset galaxy</span><br><br>');
+
+check_auth(STGC_DEVELOPER);
+
+if(!isset($_GET['sure'])) {
+    $game->out('<br><center>Do you really want to reset the galaxy COMPLETELY?<br><b><h3>All the data will be LOST FOREVER!</h3></b><br><a href="'.parse_link('a=tools/world/reset_galaxy&sure').'">Reset galaxy</a></center>');
+    return;
+}
+
+if(!isset($_GET['supersure'])) {
+    $game->out('<br><center>Let me ask you a second time: do you really, <B>REALLY</b> want to reset the galaxy COMPLETELY?<br><b><h2>All the data will be LOST FOREVER!</h2></b><br><a href="'.parse_link('a=tools/world/reset_galaxy&sure&supersure').'">Reset galaxy</a></center>');
+    return;
+}
+
+if(!isset($_GET['hypersure'])) {
+    $game->out('<br><center>Just to be sure, a third time: do you really, <b>REALLY</b>, <b><u>R E A L L Y</u></b> want to reset the galaxy COMPLETELY?<br><b><H1>ALL THE DATA WILL BE LOST FOREVER!</H1></b><br><a href="'.parse_link('a=tools/world/reset_galaxy&sure&supersure&hypersure').'">Reset galaxy</a></center>');
+    return;
+}
+
+// ###########################################################################
+// ###########################################################################
+// Ok, let's nuke the galaxy!
+
+$game->out('<b>Beginning galaxy big crunch...</b><br>');
+
+// First of all stop the tick execution and the game:
+$sql = 'UPDATE config SET tick_stopped = 1,game_stopped = 1';
+
+if(!$db->query($sql)) {
+    message(DATABASE_ERROR, 'Cannot stop tick execution!');
+}
+
+$game->out('Tick stopped<br>');
+
+// Then truncate all the existing tables except for config, FHB_debug, and skins:
+
+$game->out('Truncating DB tables...');
+
+$tables = array(
+    'account_observe',
+    'alliance',
+    'alliance_application',
+    'alliance_bposts',
+    'alliance_bthreads',
+    'alliance_diplomacy',
+    'alliance_logs',
+    'alliance_shoutbox',
+    'alliance_taxes',
+    'bidding',
+    'bidding_owed',
+    'borg_bot',
+    'borg_target',
+    'click_ids',
+    'FHB_bid_meldung',
+    'FHB_Bot',
+    'FHB_cache_trupp_trade',
+    'FHB_Handels_Lager',
+    'FHB_handel_log',
+    'FHB_logging_ship',
+    'FHB_news',
+    'FHB_ship_Lager',
+    'FHB_ship_templates',
+    'FHB_sperr_list',
+    'FHB_stats',
+    'FHB_temp_grundpreis',
+    'FHB_truppen_lib',
+    'FHB_warteschlange',
+    'future_human_reward',
+    'ip_link',
+    'logbook',
+    'message',
+    'message_archiv',
+    'message_removed',
+    'planets',
+    'planet_details',
+    'portal_news',
+    'portal_poll',
+    'portal_poll_voted',
+    'resource_trade',
+    'scheduler_instbuild',
+    'scheduler_research',
+    'scheduler_resourcetrade',
+    'scheduler_shipbuild',
+    'scheduler_shipmovement',
+    'schulden_table',
+    'settlers_relations',
+    'ships',
+    'ship_ccategory',
+    'ship_components',
+    'ship_fleets',
+    'ship_templates',
+    'ship_trade',
+    'shoutbox',
+    'spenden',
+    'starsystems',
+    'starsystems_slots',
+    'stations',
+    'tc_coords_memo',
+    'trade_settings',
+    'transport_logs',
+    'treuhandkonto',
+    'user',
+    'userally_history',
+    'user_diplomacy',
+    'user_iplog',
+    'user_logs',
+    'user_sitter_iplog',
+    'user_templates');
+
+foreach($tables as $table) {
+    if(!$db->query('TRUNCATE '.$table)) {
+        message(DATABASE_ERROR, 'Cannot truncate DB table '.$table.'!');
+    }
+}
+
+$game->out('done.<br>');
+
+// ###########################################################################
+// ###########################################################################
+// Recreate admin user's stuff
+
+$game->out('Creating STFC admin user...');
+
+// We could use some default values here...
+$sql = "INSERT INTO `user` (`user_id`, `user_active`, `user_name`, `user_loginname`, `user_password`,
+                            `user_email`, `user_auth_level`, `user_rank`, `user_override_uid`, `user_race`,
+                            `user_gfxpath`, `user_skinpath`, `user_jspath`, `user_skin`, `user_notepad`,
+                            `user_registration_time`, `user_registration_ip`, `user_attack_protection`, `user_max_colo`,
+                            `user_alliance`,`user_alliance_rights1`, `user_alliance_rights2`, `user_alliance_rights3`,
+                            `user_alliance_rights4`, `user_alliance_rights5`, `user_alliance_rights6`, `user_alliance_rights7`,
+                            `user_alliance_rights8`, `user_alliance_status`, `last_active`, `last_ip`, `user_points`,
+                            `user_planets`, `user_honor`, `user_capital`, `pending_capital_choice`, `active_planet`,
+                            `user_hidenotepad`, `globalresearch_1`, `globalresearch_2`, `globalresearch_3`, `globalresearch_4`,
+                            `globalresearch_5`, `user_last_emergency_call`, `user_vacation_start`, `user_vacation_end`,
+                            `user_last_vacation`, `user_last_vacation_duration`, `user_sitting_active`,
+                            `user_sitting_password`, `user_sitting_o1`, `user_sitting_o2`, `user_sitting_o3`,
+                            `user_sitting_o4`, `user_sitting_o5`, `user_sitting_o6`, `user_sitting_o7`, `user_sitting_o8`,
+                            `user_sitting_o9`, `user_sitting_o10`, `user_sitting_id1`, `user_sitting_id2`, `user_sitting_id3`,
+                            `user_sitting_id4`, `user_sitting_id5`, `last_tcartography_view`, `last_tcartography_id`,
+                            `last_stationatedf_dmode`, `last_movef_dmode`, `unread_messages`, `unread_log_entries`, 
+                            `unread_support_tickets`, `shoutbox_posts`, `last_shoutbox_post`, `shoutbox_flood_error`,
+                            `user_avatar`, `user_signature`, `user_icq`, `user_birthday`, `user_gender`, `user_enable_sig`,
+                            `user_rank_points`, `user_rank_planets`, `user_rank_honor`, `user_gallery_name_1`,
+                            `user_gallery_name_2`, `user_gallery_name_3`, `user_gallery_name_4`, `user_gallery_name_5`,
+                            `user_gallery_description_1`, `user_gallery_description_2`, `user_gallery_description_3`,
+                            `user_gallery_description_4`, `user_gallery_description_5`, `last_secimage`, `timeout_secimage`,
+                            `content_secimage`, `link_secimage`, `error_secimage`, `num_auctions`, `user_message_sig`,
+                            `user_options`, `plz`, `country`, `num_hits`, `num_sitting`, `language`, `tutorial`,
+                            `last_alliance_kick`, `user_trade`, `message_basement`, `trade_tick`, `notepad_width`,
+                            `notepad_cols`, `skin_farbe`) VALUES
+                           (10, 1, 'STFC-Admin', 'admin', '20de672d58473de036b873080afab62a', 'admin@stfc.it', 3, 'Developer', 0, 0,
+                           '/stfc_gfx/', 'skin1/', '', 1, '', 0, '', 3557,
+                           0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1343032585, '10.1.8.37',
+                           9, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '',
+                           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+                           1489, 0, 0, 0, 0, 0, 273, 1335786338, 9, '', '', '', '', '-', 0, 2, 2, 1,
+                           'Spazio libero', 'Spazio libero', 'Spazio libero', 'Spazio libero', 'Spazio libero', '', '', '', '', '',
+                           1343028872, 1276633833, '', 'tmpsec/sec0558eba1efd5812e49a30c7f75acb37e.gif', 0, 1, '',
+                           'a:9:{s:15:\"planetlist_show\";i:0;s:16:\"planetlist_order\";i:0;s:22:\"alliance_status_member\";i:2;s:16:\"redalert_options\";i:0;s:10:\"show_trade\";i:1;s:6:\"type_2\";i:1;s:6:\"type_0\";i:1;s:6:\"type_3\";i:1;s:6:\"type_1\";i:1;}',
+                           '74586', 'IT', 114, 0, 'ITA', 0, 8566, 3, '', 0, 200, 13, '')";
+
+if(!$db->query($sql)) {
+    message(DATABASE_ERROR, 'Cannot create STFC-Admin user!');
+}
+
+$game->out('done.<br>Creating STFC admin star system...');
+
+$sql = 'INSERT INTO `starsystems` (
+            `system_id`, `system_name`, `sector_id`,
+            `system_x`, `system_y`,
+            `system_map_x`, `system_map_y`,
+            `system_global_x`, `system_global_y`,
+            `system_starcolor_red`, `system_starcolor_green`, `system_starcolor_blue`,
+            `system_starsize`,
+            `system_n_planets`,
+            `system_max_planets`,
+            `system_closed`)
+        VALUES (
+            1, "System E14:F2", 122,
+            2, 6,
+            52, 229,
+            119, 42,
+            18, 21, 165,
+            12,
+            1,
+            6,
+            1)';
+
+if(!$db->query($sql)) {
+    message(DATABASE_ERROR, 'Cannot create STFC-Admin starsystem!');
+}
+
+$game->out('done.<br>Creating STFC admin planet...');
+
+$sql = 'INSERT INTO `planets` (
+            `planet_id`, `planet_name`, `system_id`, `sector_id`,
+            `planet_owner`, `planet_owned_date`, `planet_distance_id`,
+            `planet_distance_px`, `planet_covered_distance`, `planet_tick_cdistance`,
+            `planet_max_cdistance`, `planet_current_x`, `planet_current_y`,
+            `planet_points`, `planet_available_points`,`planet_thumb`,
+            `resource_1`, `resource_2`, `resource_3`, `resource_4`,
+            `rateo_1`, `rateo_2`, `rateo_3`, `rateo_4`, `recompute_static`,
+            `max_resources`, `max_worker`, `max_units`,
+            `workermine_1`, `workermine_2`, `workermine_3`,
+            `planet_altname`)
+        VALUES (
+            1, "Admin Home", 1, 122,
+            10, '.$game->TIME.', 4,
+            146, 0, 19,
+            917, 0, 0,
+            10, 1173, "",
+            200, 200, 100, 100,
+            1, 1, 1, 1, 1,
+            38000, 65000, 65000,
+            100, 100, 100,
+            "")';
+
+if(!$db->query($sql)) {
+    message(DATABASE_ERROR, 'Cannot create STFC-Admin planet!');
+}
+
+$game->out('done.<br>Creating STFC admin user template...');
+
+$file = $config['game_url'].'/modules/tools/world/default_template.html';
+if(($template = file_get_contents($file)) === false) {
+    message(GENERAL, 'Cannot open default template file: '.$file);
+}
+
+$template = utf8_decode($template);
+$sql =  'INSERT INTO `user_templates` (`user_id`, `user_template`) VALUES
+                                     (10, "'.addslashes($template).'")';
+
+if(!$db->query($sql)) {
+    message(DATABASE_ERROR, 'Cannot create STFC-Admin user template!');
+}
+
+// ###########################################################################
+// ###########################################################################
+// Recreate free starsystems slots
+
+$game->out('done.<br>Recreate free starsystems slots...');
+
+$_GET['sure'] = 1;
+include('repair_starsystem_slots.php');
+
+$game->out('done.<br>Reset config table to initial status...');
+
+$sql = 'UPDATE config
+        SET tick_id = 1,
+            tick_time = '.time().',
+            stardate = 21000.0,
+            ferengitax_1 = 0,
+            ferengitax_2 = 0,
+            ferengitax_3 = 0,
+            last_paytime = 0,
+            future_ship = 0,
+            settler_tmp_1 = 0,
+            settler_tmp_2 = 0,
+            settler_tmp_3 = 0,
+            settler_tmp_4 = 0';
+
+            if(!$db->query($sql)) {
+    message(DATABASE_ERROR, 'Cannot reset config table to initial status!');
+}
+
+$game->out('done.<br><b>Galaxy has been successfully reset.</b>');
+
+// ###########################################################################
+// ###########################################################################
+// Remember the admin to install the BOTS, at least until the procedure
+// will be automated in this script.
+$game->out('<br><br><h3>Remember to install the BOTs BEFORE starting the tick!</h3>');
+
+?>
