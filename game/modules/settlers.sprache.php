0a36956c4df017b91f3eb1ecb846f25dd66ad39b
kail72 <cristiano.delogu.72@gmail.com>
2014-07-19 11:05:48 +0000

    Modulo di gestione rapporti con Colonie Indipendenti. Testo interfaccia.

---
+++ game/modules/settlers.sprache.php
@@ -0,0 +1,73 @@
+<?php
+/*    
+	This file is part of STFC.
+	Copyright 2006-2007 by Michael Krauss (info@stfc2.de) and Tobias Gafner
+		
+	STFC is based on STGC,
+	Copyright 2003-2007 by Florian Brede (florian_brede@hotmail.com) and Philipp Schmidt
+	
+    STFC is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    STFC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+//TEDESCO
+define ("GER_TEXT0",'Settlers Colonies:');
+
+//INGLESE
+define ("ENG_TEXT0",'Settlers Colonies:');
+
+// ITALIANO
+define ("ITA_TEXT0",'Colonie Indipendenti');
+
+define ("ITA_TEXT1", 'Relazioni');
+
+define ("ITA_TEXT2", 'Fondatore');
+
+define ("ITA_TEXT3", 'Missioni');
+
+define ("ITA_TEXT4", 'Eventi');
+
+define ("ITA_TEXT5", 'Addestramenti');
+
+define ("ITA_TEXT10", 'Nome');
+
+define ("ITA_TEXT11", 'Posizione');
+
+define ("ITA_TEXT12", 'Classe');
+
+define ("ITA_TEXT13", 'Valore relazioni');
+
+define ("ITA_TEXT14", 'Pianeta Collegato');
+
+define ("ITA_TEXT15", 'nessuno');
+
+define ("ITA_TEXT16", 'Link');
+
+define ("ITA_TEXT17", 'UnLink');
+
+define ("ITA_TEXT18", 'Missione');
+
+define ("ITA_TEXT19", 'Nave');
+
+define ("ITA_TEXT20", 'In vita');
+
+define ("ITA_TEXT21", 'Attiva');
+
+define ("ITA_TEXT22", 'Riepilogo');
+
+define ("ITA_TEXT23", 'Valore');
+
+define ("ITA_TEXT24", 'Alleato');
+
+define ("ITA_TEXT25", 'Liv.');
+
+?>
