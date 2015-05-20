8710414ef9b6ecded7d2b338685f8d3c7c0532f3
kirirur <kirirur@gmail.com>
2015-05-20 10:07:45 +0200

    Revert "New multi-line shipyard module. Now a planet can have up to four different lines of production, based on the planet itself."
    This reverts commit 8d9ce01a235716ab5dd10734c1df4fc639573f92.

---
+++ game/modules/shipyard.sprache.php
@@ -0,0 +1,492 @@
+<?php
+/*    
+	This file is part of STFC.
+	Copyright 2006-2007 by Michael Krauss (info@stfc2.de) and Tobias Gafner
+		
+	STFC is based on STGC,
+	Copyright 2003-2007 by Florian Brede (florian_brede@hotmail.com) and Philipp Schmidt
+	
+    STFC is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    STFC is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//deutsch
+define ("GER_TEXT0", 'Du ben&ouml;tigst');
+
+define ("GER_TEXT1", 'Gesamtpunkte.');
+
+define ("GER_TEXT2", 'Planetenpunkte.');
+
+define ("GER_TEXT3", 'Name:');
+
+define ("GER_TEXT4", 'Beschreibung:');
+
+define ("GER_TEXT5", 'Bild:');
+
+define ("GER_TEXT6", 'Komponenten:');
+
+define ("GER_TEXT7", '- Nicht belegt<br>');
+
+define ("GER_TEXT8", 'Schiffsdaten:');
+
+define ("GER_TEXT9", 'L. Waffen:');
+
+define ("GER_TEXT10", 'Schw. Waffen:');
+
+define ("GER_TEXT11", 'Pl. Waffen:');
+
+define ("GER_TEXT12", 'Schildst&auml;rke:');
+
+define ("GER_TEXT13", 'H&uuml;lle (HP):');
+
+define ("GER_TEXT14", 'Reaktion:');
+
+define ("GER_TEXT15", 'Bereitschaft:');
+
+define ("GER_TEXT16", 'Wendigkeit:');
+
+define ("GER_TEXT17", 'Erfahrung:');
+
+define ("GER_TEXT18", 'Warp:');
+
+define ("GER_TEXT19", 'Sensoren:');
+
+define ("GER_TEXT20", 'Tarnung:');
+
+define ("GER_TEXT21", 'Energieverbrauch:');
+
+define ("GER_TEXT22", 'Ressourcen + Standardcrew:');
+
+define ("GER_TEXT23", 'Bauzeit:');
+
+define ("GER_TEXT24", 'Minimale Besatzung:');
+
+define ("GER_TEXT25", 'Fehler: Datenbankfehler in Modul shipyard ( function: Abort_Build() ); bitte Admin melden!');
+
+define ("GER_TEXT26", '-&nbsp;Zu wenige Standardressourcen<br>');
+
+define ("GER_TEXT27", '-&nbsp;Zu wenige Einheiten<br>');
+
+define ("GER_TEXT28", '-&nbsp;Baubedingungen nicht erf&uuml;llt<br>');
+
+define ("GER_TEXT29", 'Fehler: Bau konnte nicht gestartet werden');
+
+define ("GER_TEXT30", 'Baustatus');
+
+define ("GER_TEXT31", 'Gebaut wird:');
+
+define ("GER_TEXT32", 'Verbleibende Zeit:');
+
+define ("GER_TEXT33", 'Alle Bauauftr&auml;ge abbrechen');
+
+define ("GER_TEXT34", 'Warteschlange:');
+
+define ("GER_TEXT35", 'Standardansicht');
+
+define ("GER_TEXT36", 'Fertig:');
+
+define ("GER_TEXT37", 'Uhr');
+
+define ("GER_TEXT38", 'Schiffe');
+
+define ("GER_TEXT39", 'Detailansicht');
+
+define ("GER_TEXT40", 'Aufgrund der Tickzeiten dauert der erste Bauvorgang u.U. bis zu 3 Minuten l&auml;nger.');
+
+define ("GER_TEXT41", 'Verf&uuml;gbare Truppen:');
+
+define ("GER_TEXT42", 'Angriff:');
+
+define ("GER_TEXT43", 'Standard:');
+
+define ("GER_TEXT44", 'Verteidigung:');
+
+define ("GER_TEXT45", 'Schiffe in Auftrag geben');
+
+define ("GER_TEXT46", 'Besatzung f&uuml;r Schiff(e)');
+
+define ("GER_TEXT47", 'w&auml;hlen:');
+
+define ("GER_TEXT48", '* Info: Die Zahl in den Eingabefeldern repr&auml;sentiert die max. verf&uuml;gbare Anzahl der Einheiten pro Schiff');
+
+define ("GER_TEXT49", 'Maximal verf&uuml;gbare Einheiten w&auml;hlen');
+
+define ("GER_TEXT50", 'Bau starten');
+
+define ("GER_TEXT51", 'Schiffstemplate:');
+
+define ("GER_TEXT52", 'Dauer pro Schiff:');
+
+define ("GER_TEXT53", 'Anzahl:');
+
+define ("GER_TEXT54", 'Start');
+
+define ("GER_TEXT55", '* Info: Die Zahl in den Eingabefeldern repr&auml;sentiert die max. verf&uuml;gbare Anzahl von Schiffen die gebaut werden k&ouml;nnen<br><br>Wenn du mit dem Mauszeiger &uuml;ber ein Schiff f&auml;hrst kannst du sehen, ob alle Komponenten auf dem Planeten zum Bau erforscht sind.');
+
+define ("GER_TEXT56", 'Rot => nicht erforscht');
+
+define ("GER_TEXT57", 'Gr&uuml;n => erforscht');
+
+define ("GER_TEXT58", 'Du musst erst Schiffe unter "Schiffstemplates" zusammenstellen, bevor du Schiffe in Auftrag geben kannst.');
+
+define ("GER_TEXT59", 'Du musst ein(e)');
+
+define ("GER_TEXT60", 'bauen, bevor Schiffe gebaut werden k&ouml;nnen.');
+
+define ("GER_TEXT61", 'Du kannst auf diesen Bereich nicht zugreifen, weil du nur');
+
+define ("GER_TEXT62", 'der n&ouml;tigen Sicherheitstruppen auf diesem Planeten hast.<br>Ein Zugriff ist ab <b>70%</b> wieder m&ouml;glich.<br><br><i><u>Hinweis:</u> Aktive Bauvorg&auml;nge laufen noch weiter und k&ouml;nnen z.B. im Hauptquartier eingesehen werden.</i>');
+
+define ("GER_TEXT63", 'Metall');
+
+define ("GER_TEXT64", 'Mineral');
+
+define ("GER_TEXT65", 'Latinum');
+
+define ("GER_TEXT66", 'Arbeiter');
+
+define ("GER_TEXT67", 'Fehlende Ressourcen');
+
+define ("GER_TEXT68", 'Arbeitsleitung eins:');
+
+define ("GER_TEXT69", 'Arbeitsleitung zwei:');
+
+define ("GER_TEXT70", 'Arbeitsleitung drei:');
+
+define ("GER_TEXT71", 'Arbeitsleitung vier:');
+
+define ("GER_TEXT72", 'Zivil R&uuml;mpfe');
+
+define ("GER_TEXT73", 'Milit&auml;r Rumpf Typ 1');
+
+define ("GER_TEXT74", 'Milit&auml;r Rumpf Typ 2');
+
+define ("GER_TEXT75", 'Milit&auml;r Rumpf Typ 3');
+
+define ("GER_TEXT76", 'Schiffe in der Warteschlange');
+
+
+//English
+define ("ENG_TEXT0", 'You need');
+
+define ("ENG_TEXT1", 'Total points');
+
+define ("ENG_TEXT2", 'Planetary points.');
+
+define ("ENG_TEXT3", 'Name:');
+
+define ("ENG_TEXT4", 'Description:');
+
+define ("ENG_TEXT5", 'Image:');
+
+define ("ENG_TEXT6", 'Components:');
+
+define ("ENG_TEXT7", '- Not used<br>');
+
+define ("ENG_TEXT8", 'Ship\'s Data:');
+
+define ("ENG_TEXT9", 'L. weapons:');
+
+define ("ENG_TEXT10", 'H. weapons:');
+
+define ("ENG_TEXT11", 'Pl. weapons:');
+
+define ("ENG_TEXT12", 'Shield strength:');
+
+define ("ENG_TEXT13", 'Shell (HP):');
+
+define ("ENG_TEXT14", 'Reaction:');
+
+define ("ENG_TEXT15", 'Readiness:');
+
+define ("ENG_TEXT16", 'Agility:');
+
+define ("ENG_TEXT17", 'Experience:');
+
+define ("ENG_TEXT18", 'Warp:');
+
+define ("ENG_TEXT19", 'Sensors:');
+
+define ("ENG_TEXT20", 'Camouflage:');
+
+define ("ENG_TEXT21", 'Energy consumption:');
+
+define ("ENG_TEXT22", 'Resources + Standardcrew:');
+
+define ("ENG_TEXT23", 'Construction period:');
+
+define ("ENG_TEXT24", 'Minimum crew:');
+
+define ("ENG_TEXT25", 'Error: error in the database module shipyard (function: Abort_Build ()); please report to an admin!');
+
+define ("ENG_TEXT26", '-&nbsp;Too few standard resources<br>');
+
+define ("ENG_TEXT27", '-&nbsp;Too few units<br>');
+
+// Alternative: "Building conditions not fulfilled" o "does not fulfill building conditions"
+define ("ENG_TEXT28", '-&nbsp;Building conditions does not fulfill<br>');
+
+define ("ENG_TEXT29", 'Error: construction could not start');
+
+define ("ENG_TEXT30", 'Building status');
+
+define ("ENG_TEXT31", 'Will be constructed:');
+
+define ("ENG_TEXT32", 'Remaining time:');
+
+define ("ENG_TEXT33", 'Cancel all contracts');
+
+define ("ENG_TEXT34", 'Queue:');
+
+define ("ENG_TEXT35", 'Standard view');
+
+define ("ENG_TEXT36", 'Completion:');
+
+define ("ENG_TEXT37", 'PM');
+
+define ("ENG_TEXT38", 'Ship');
+
+define ("ENG_TEXT39", 'Detail view');
+
+define ("ENG_TEXT40", 'Due to the ticking times the first building procedure lasts possibly up to 3 minutes longer.');
+
+define ("ENG_TEXT41", 'Available troops:');
+
+define ("ENG_TEXT42", 'Attack:');
+
+define ("ENG_TEXT43", 'Standard:');
+
+define ("ENG_TEXT44", 'Defense:');
+
+define ("ENG_TEXT45", 'Ship in order');
+
+define ("ENG_TEXT46", 'Crew of ship (s)');
+
+define ("ENG_TEXT47", 'Select:');
+
+define ("ENG_TEXT48", '* Info: The number in the input fields represents the max. available number of units per ship');
+
+define ("ENG_TEXT49", 'Select maximum available units');
+
+define ("ENG_TEXT50", 'Start construction');
+
+define ("ENG_TEXT51", 'Ship template:');
+
+define ("ENG_TEXT52", 'Duration per ship:');
+
+define ("ENG_TEXT53", 'Number:');
+
+define ("ENG_TEXT54", 'Start');
+
+define ("ENG_TEXT55", '* Info: The number in the input fields represents the max. available number of ships that can be built<br><br>If you place the mouse pointer over a ship you can see whether all components on the planet are researched to the building.');
+
+define ("ENG_TEXT56", 'Red => not researched');
+
+define ("ENG_TEXT57", 'Green => researched');
+
+define ("ENG_TEXT58", 'You must have arrange ships under "Ship template", before you can give ships in order.');
+
+define ("ENG_TEXT59", 'You must have one');
+
+define ("ENG_TEXT60", 'built before ships can be build.');
+
+define ("ENG_TEXT61", 'You cannot access this area, because you have only');
+
+define ("ENG_TEXT62", 'of the necessary safety troops on this planet.<br>An access is again possible starting from <b>70%</b>.<br><br><i><u>Note</u>: Active building procedures still keep running and can be seen e.g. in headquarters.</i>');
+
+define ("ENG_TEXT63", 'Metal');
+
+define ("ENG_TEXT64", 'Minerals');
+
+define ("ENG_TEXT65", 'Dilithium');
+
+define ("ENG_TEXT66", 'Workers');
+
+define ("ENG_TEXT67", 'Missing resources');
+
+define ("ENG_TEXT68", 'Working Line One:');
+
+define ("ENG_TEXT69", 'Working Line Two:');
+
+define ("ENG_TEXT70", 'Working Line Three:');
+
+define ("ENG_TEXT71", 'Working Line Four:');
+
+define ("ENG_TEXT72", 'Civilian Hulls');
+
+define ("ENG_TEXT73", 'Type 1 Military Hulls');
+
+define ("ENG_TEXT74", 'Type 2 Military Hulls');
+
+define ("ENG_TEXT75", 'Type 3 Military Hulls');
+
+define ("ENG_TEXT76", 'Ships in the queue');
+
+
+//Italiano
+define ("ITA_TEXT0", 'Necessiti');
+
+define ("ITA_TEXT1", 'Punti Totali');
+
+define ("ITA_TEXT2", 'Punti Planetari.');
+
+define ("ITA_TEXT3", 'Nome:');
+
+define ("ITA_TEXT4", 'Descrizione:');
+
+define ("ITA_TEXT5", 'Immagine:');
+
+define ("ITA_TEXT6", 'Componenti:');
+
+define ("ITA_TEXT7", '- non installato<br>');
+
+define ("ITA_TEXT8", 'Dati della Nave:');
+
+define ("ITA_TEXT9", 'Armi Leggere:');
+
+define ("ITA_TEXT10", 'Armi Pesanti:');
+
+define ("ITA_TEXT11", 'Armi Planetarie:');
+
+define ("ITA_TEXT12", 'Potenza Scudi:');
+
+define ("ITA_TEXT13", 'Punti Scafo (HP):');
+
+define ("ITA_TEXT14", 'Reazione:');
+
+define ("ITA_TEXT15", 'Prontezza:');
+
+define ("ITA_TEXT16", 'Agilit&agrave;:');
+
+define ("ITA_TEXT17", 'Esperienza:');
+
+define ("ITA_TEXT18", 'Curvatura (Warp):');
+
+define ("ITA_TEXT19", 'Sensori:');
+
+define ("ITA_TEXT20", 'Occultamento:');
+
+define ("ITA_TEXT21", 'Energia usata:');
+
+define ("ITA_TEXT22", 'Risorse + Equip.standard:');
+
+define ("ITA_TEXT23", 'Tempo di Costruzione:');
+
+define ("ITA_TEXT24", 'Equipaggio Minimo:');
+
+define ("ITA_TEXT25", 'Errore: errore nel modulo di database del Cantiere Navale (funzione: Abort_Build ()); si prega di segnalarlo ad un admin!');
+
+define ("ITA_TEXT26", '-&nbsp;Poche risorse standard<br>');
+
+define ("ITA_TEXT27", '-&nbsp;Poche unit&agrave;<br>');
+
+// Alternative: "Building conditions not fulfilled" o "does not fulfill building conditions"
+define ("ITA_TEXT28", '-&nbsp;Condizioni di costruzione non soddisfatte<br>');
+
+define ("ITA_TEXT29", 'Errore: la costruzione non pu&ograve; iniziare');
+
+define ("ITA_TEXT30", 'Stato costruzione');
+
+define ("ITA_TEXT31", 'Sar&agrave; costruito:');
+
+define ("ITA_TEXT32", 'Tempo rimanente:');
+
+define ("ITA_TEXT33", 'Interrompi');
+
+define ("ITA_TEXT34", 'Coda:');
+
+define ("ITA_TEXT35", 'Visuale Standard');
+
+define ("ITA_TEXT36", 'Completamento:');
+
+define ("ITA_TEXT37", 'PM');
+
+define ("ITA_TEXT38", 'Nave');
+
+define ("ITA_TEXT39", 'Visualizza Dettagli');
+
+define ("ITA_TEXT40", 'A causa dei cicli di tick la procedura di costruzione potrebbe durare fino a 3 minuti in pi&ugrave;.');
+
+define ("ITA_TEXT41", 'Truppe:');
+
+define ("ITA_TEXT42", 'Attacco:');
+
+define ("ITA_TEXT43", 'Standard:');
+
+define ("ITA_TEXT44", 'Difesa:');
+
+define ("ITA_TEXT45", 'Navi costruibili');
+
+define ("ITA_TEXT46", 'Seleziona equipaggio nave(i)');
+
+define ("ITA_TEXT47", ':');
+
+define ("ITA_TEXT48", '* Info: Il numero nei campi di input rappresenta il max. numero di unit&agrave; disponibili per ogni nave');
+
+define ("ITA_TEXT49", 'Seleziona il massimo numero disponibile di unit&agrave;');
+
+define ("ITA_TEXT50", 'Inizia costruzione');
+
+define ("ITA_TEXT51", 'Progetto Nave:');
+
+define ("ITA_TEXT52", 'Durata per Nave:');
+
+define ("ITA_TEXT53", 'Numero:');
+
+define ("ITA_TEXT54", 'Inizia');
+
+define ("ITA_TEXT55", '* Info: Il numero nei campi di input rappresenta il max. numero di navi che possono essere costruite<br><br>Se sposti il puntatore del mouse sopra una nave puoi vedere se tutti i componenti sono stati ricercati sul pianeta per la costruzione.');
+
+define ("ITA_TEXT56", 'Rosso => non ricercato');
+
+define ("ITA_TEXT57", 'Verde => ricercato');
+
+define ("ITA_TEXT58", 'Devi disporre di navi sotto "Progetti navi" prima di poterne costruire.');
+
+define ("ITA_TEXT59", 'Devi avere un');
+
+define ("ITA_TEXT60", 'costruito, prima che le navi possano essere create.');
+
+define ("ITA_TEXT61", 'Non puoi accedere a quest&#146;area, perch&egrave; hai solo');
+
+define ("ITA_TEXT62", 'delle truppe necessarie alla sicurezza su questo pianeta.<br>Un accesso sar&agrave; nuovamente possibile partendo dal <b>70%</b>.<br><br><i><u>Nota</u>: Le procedure di costruzione in corso rimangono attive e possono essere monitorate dal Quartier Generale</i>');
+
+define ("ITA_TEXT63", 'Metallo');
+
+define ("ITA_TEXT64", 'Minerali');
+
+define ("ITA_TEXT65", 'Dilitio');
+
+define ("ITA_TEXT66", 'Lavoratori');
+
+define ("ITA_TEXT67", 'Risorse mancanti');
+
+define ("ITA_TEXT68", 'Linea Uno:');
+
+define ("ITA_TEXT69", 'Linea Due:');
+
+define ("ITA_TEXT70", 'Linea Tre:');
+
+define ("ITA_TEXT71", 'Linea Quattro:');
+
+define ("ITA_TEXT72", 'Scafi Civili');
+
+define ("ITA_TEXT73", 'Scafi Militari Tipo 1');
+
+define ("ITA_TEXT74", 'Scafi Militari Tipo 2');
+
+define ("ITA_TEXT75", 'Scafi Militari Tipo 3');
+
+define ("ITA_TEXT76", 'Navi in coda di costruzione');
+
+?>
