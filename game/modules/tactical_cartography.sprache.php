15b26ff6b627f51feecadef41eb54cd9e4c2d0b2
Kail <cristiano.delogu.72@gmail.com>
2015-10-03 22:37:37 +0200

    "Send here" panel now shows fleet distance from the displayed planet in A.U.

--- game/modules/tactical_cartography.sprache.php
+++ game/modules/tactical_cartography.sprache.php
@@ -290,6 +290,10 @@ define ("GER_TEXT130", ' startet eine neue Kolonie!');
 
 define ("GER_TEXT131", '<span style="color: #CC6600;">orange</span> - unabh&auml;ngige Kolonie<br>');
 
+define ("GER_TEXT132", 'Systeme');
+
+define ("GER_TEXT133", 'distance: ');
+
 
 //English
 define ("ENG_TEXT0", 'Tactical headquarters:');
@@ -559,6 +563,10 @@ define ("ENG_TEXT129", ' a colony ship of ');
 define ("ENG_TEXT130", ' starts a new independent colony!');
 
 define ("ENG_TEXT131", '<span style="color: #CC6600;">arancio</span> - Pianeta Indipendente<br>');
+
+define ("ENG_TEXT132", 'System');
+
+define ("ENG_TEXT133", 'distance: ');
 
 //Italiano
 define ("ITA_TEXT0", 'Comando Tattico:');
@@ -830,5 +838,9 @@ define ("ITA_TEXT129", ' una unit&agrave; colonizzatrice di ');
 define ("ITA_TEXT130", ' realizza una colonia indipendente!');
 
 define ("ITA_TEXT131", '<span style="color: #CC6600;">arancio</span> - Pianeta Indipendente<br>');
+
+define ("ITA_TEXT132", 'Sistema');
+
+define("ITA_TEXT133", 'distanza: ');
 
 ?>
