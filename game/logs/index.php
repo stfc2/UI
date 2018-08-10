4ec9507f2ae7015836e8b57f0b5c3689b4ef02dc
Andrea Carolfi <kirirur@gmail.com>
2018-08-10 14:20:51 +0200

    Added index page to logs directory
    Added index.php web page (pure HTML in reality) to logs directory.

---
+++ game/logs/index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+
+<HTML>
+<TITLE>
+Star Trek: Frontline Combat - Logs page 
+</TITLE>
+<BDOY>
+<H1>Here you can display STFC daily logs.</H1>
+Available logs:<br>
+<A HREF="view_log.php">Scheduler</A><br>
+<A HREF="view_moves_log.php">Moves</a><br>
+<A HREF="view_log_NPC.php">Non Playing Components</a><br>
+<hr>
+<A HREF="fixall/view_log.php">Fix all</A><br>
+<A HREF="sixhours/view_log.php">Six hours</a><br>
+<hr>
+<A HREF="view_log.php?file=daily">Daily</A><br>
+<A HREF="view_log.php?file=NPC_installation">NPC installation</A><br>
+</BODY>
+</HTML>
